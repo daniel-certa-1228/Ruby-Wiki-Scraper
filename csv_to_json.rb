4024a928ba3db0e3a82a8ec3267432ca4875b880
daniel-certa-1228 <mfdancerta@gmail.com>
2018-02-19 14:47:38 -0600

    saved to json

--- csv_to_json.rb
+++ csv_to_json.rb
@@ -2,4 +2,9 @@ require 'json'
 require 'csv'
 
 lines = CSV.read('beatles_songs.csv')
-puts lines
+# puts lines
+# beatles_json = lines.to_json
+# puts beatles_json
+File.open('beatles_songs.json', 'w') do |f|
+    f.write(lines.to_json)
+end
