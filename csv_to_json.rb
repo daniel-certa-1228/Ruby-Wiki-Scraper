f01d1a5940f8f68e82d21bb0aa56222994ebef58
daniel-certa-1228 <mfdancerta@gmail.com>
2018-02-19 16:57:43 -0600

    better json with more info

--- csv_to_json.rb
+++ csv_to_json.rb
@@ -1,14 +1,36 @@
 require 'json'
 require 'csv'
 
+# json 1, clean array of song tiles
 beatles_songs = []
 
 CSV.foreach('beatles_songs.csv') do |line|
     beatles_songs.push(line[0])
 end
 beatles_songs.shift
-# puts beatles_songs.inspect
 
 File.open('beatles_songs.json', 'w') do |f|
     f.write(beatles_songs.to_json)
 end
+
+#json 2, has more data
+beatles_songs_2 = []
+lines = CSV.read('beatles_songs.csv')
+lines.each do |song, album, songwriter, lead_vocal, year, notes, ref|
+    song_hash = Hash.new
+    song_hash[:song_title] = song
+    song_hash[:album] = album
+    song_hash[:songwriters] = songwriter
+    song_hash[:lead_vocals] = lead_vocal
+    song_hash[:year] = year
+    
+    beatles_songs_2.push(song_hash)
+end
+beatles_songs_2.shift
+beatles_song_hash = Hash.new
+beatles_song_hash[:beatle_songs] = beatles_songs_2
+# puts beatles_song_hash.inspect
+File.open('beatles_songs_2.json', 'w') do |f|
+    f.write(beatles_song_hash.to_json)
+end
+
